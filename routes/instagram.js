8a629deb281a06ac42902fc3475adb600f761b36
Scott Rhodie <scottrhodie@Scotts-MacBook-Pro.local>
2018-12-20 18:47:28 +0100

    Refactoring code.  Functions moved to helpers folder

--- routes/instagram.js
+++ routes/instagram.js
@@ -1,50 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const Joi = require('joi'); // problem?
 
-const instagram = require('../mock-database')
+const instagram = require('../mock-database');
 
-function validateInfo(inVal) {
-	const schema = {
-		name: Joi.string()
-			.min(2)
-			.required(),
-		photo: Joi.string()
-			.min(6)
-			.required(),
-		likes: Joi.number()
-			.min(1)
-			.required(),
-	};
+const helpers = require('../helpers');
 
-	return Joi.validate(inVal, schema);
-}
-
-const findUserByName = name => instagram.find(currentUser => currentUser.name === name);
-const findUserById = id => instagram.find(currentUser => currentUser.id === parseInt(id));
+// const findUserByName = name => instagram.find(currentUser => currentUser.name === name);
 
 router.get('/', (req, res) => {
 	const {
 		name
 	} = req.query;
 	if (!name) return res.send(instagram);
-	const user = findUserByName(name);
+	const user = helpers.findUserByName(instagram, name);
 	if (!user) return res.status(404).send('The user name was not found.');
 	res.send(user);
 });
 
 router.get('/:id', (req, res) => {
-	const getId = findUserById(req.params.id);
+	const getId = helpers.findUserById(instagram, req.params.id);
 	if (!getId) return res.status(404).send('The user ID was not found.');
 	res.send(getId);
 });
 
 router.put('/:id', (req, res) => {
-	const putUser = findUserById(req.params.id);
+	const putUser = helpers.findUserById(instagram, req.params.id);
 	if (!putUser) return res.status(404).send('Unable to edit user.  The ID was not found.');
 	const {
 		error
-	} = validateInfo(req.body);
+	} = helpers.validateInfo(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
 	putUser.name = req.body.name;
@@ -56,7 +40,7 @@ router.put('/:id', (req, res) => {
 router.post('/', (req, res) => {
 	const {
 		error
-	} = validateInfo(req.body); // result.error
+	} = helpers.validateInfo(req.body); // result.error
 	if (error) return res.status(400).send(error.details[0].message);
 
 	const newUser = {
@@ -70,7 +54,7 @@ router.post('/', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-	const deleteUser = findUserById(req.params.id);
+	const deleteUser = helpers.findUserById(instagram, req.params.id);
 	if (!deleteUser) return res.status(404).send('The user ID was not found.');
 
 	const index = instagram.indexOf(deleteUser);
@@ -80,3 +64,4 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
