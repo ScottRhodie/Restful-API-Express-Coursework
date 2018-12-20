70e9edb326e3422ba5d9552910ff57d632f23639
Scott Rhodie <scottrhodie@Scotts-MacBook-Pro.local>
2018-12-20 17:31:19 +0100

    Refactored code a little to have a seperate file to store the database

--- routes/instagram.js
+++ routes/instagram.js
@@ -1,27 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const Joi = require('joi');  // problem?
+const Joi = require('joi'); // problem?
 
-const instagram = [
-	{ id: 1, name: 'Alan', photo: 'http://instagram.com/images/abc22343.png', likes: 45 },
-	{ id: 2, name: 'Becca', photo: 'http://instagram.com/images/abc28433.png', likes: 5 },
-	{ id: 3, name: 'Chris', photo: 'http://instagram.com/images/abc25673.png', likes: 8 },
-	{ id: 4, name: 'Darren', photo: 'http://instagram.com/images/abc324523.png', likes: 58 },
-	{ id: 5, name: 'Frank', photo: 'http://instagram.com/images/abc27253.png', likes: 123 },
-	{ id: 6, name: 'Gary', photo: 'http://instagram.com/images/abc2395.png', likes: 47 },
-	{ id: 7, name: 'Harry', photo: 'http://instagram.com/images/abc253473.png', likes: 41 },
-	{ id: 8, name: 'Ingrid', photo: 'http://instagram.com/images/abc226753.png', likes: 1 },
-	{ id: 9, name: 'Joelle', photo: 'http://instagram.com/images/abc284733.png', likes: 8 },
-	{ id: 10, name: 'Kylie', photo: 'http://instagram.com/images/abc214563.png', likes: 9 },
-	{ id: 11, name: 'Laura', photo: 'http://instagram.com/images/abc22363.png', likes: 24 },
-	{ id: 12, name: 'Mike', photo: 'http://instagram.com/images/abc265.png', likes: 6 },
-	{ id: 13, name: 'Nicolas', photo: 'http://instagram.com/images/abc6234.png', likes: 1349238 },
-	{ id: 14, name: 'Peter', photo: 'http://instagram.com/images/abc22854.png', likes: 5258 },
-	{ id: 15, name: 'Roberto', photo: 'http://instagram.com/images/abc5123.png', likes: 8 },
-	{ id: 16, name: 'Steven', photo: 'http://instagram.com/images/abc29837.png', likes: 418 },
-	{ id: 17, name: 'Trish', photo: 'http://instagram.com/images/abc29267804.png', likes: 4758 },
-	{ id: 18, name: 'William', photo: 'http://instagram.com/images/abcpy98234.png', likes: 8 },
-];
+const instagram = require('../mock-database')
 
 function validateInfo(inVal) {
 	const schema = {
@@ -43,7 +24,9 @@ const findUserByName = name => instagram.find(currentUser => currentUser.name ==
 const findUserById = id => instagram.find(currentUser => currentUser.id === parseInt(id));
 
 router.get('/', (req, res) => {
-	const { name } = req.query;
+	const {
+		name
+	} = req.query;
 	if (!name) return res.send(instagram);
 	const user = findUserByName(name);
 	if (!user) return res.status(404).send('The user name was not found.');
@@ -59,7 +42,9 @@ router.get('/:id', (req, res) => {
 router.put('/:id', (req, res) => {
 	const putUser = findUserById(req.params.id);
 	if (!putUser) return res.status(404).send('Unable to edit user.  The ID was not found.');
-	const { error } = validateInfo(req.body);
+	const {
+		error
+	} = validateInfo(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
 	putUser.name = req.body.name;
@@ -69,18 +54,20 @@ router.put('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    const { error } = validateInfo(req.body);  // result.error
-    if (error) return res.status(400).send(error.details[0].message);
+	const {
+		error
+	} = validateInfo(req.body); // result.error
+	if (error) return res.status(400).send(error.details[0].message);
 
-    const newUser = {
-        id: instagram.length + 1, 
+	const newUser = {
+		id: instagram.length + 1,
 		name: req.body.name,
 		photo: req.body.photo,
 		likes: req.body.likes
-    };
-    instagram.push(newUser);
-    res.send(newUser);
-})
+	};
+	instagram.push(newUser);
+	res.send(newUser);
+});
 
 router.delete('/:id', (req, res) => {
 	const deleteUser = findUserById(req.params.id);
