3e91577e3c1565949c6f8ba07e4317a95925e52c
ScottRhodie <45167289+ScottRhodie@users.noreply.github.com>
2019-01-15 11:01:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-// Call expressJS and Joi Validation
+const Joi = require('joi');
+const home = require('./routes/home')
+const instagram = require('./routes/instagram')
 const express = require('express');
 const app = express();
-const Joi = require('joi');
 
-// Use body parsing
 app.use(express.json());
-
-// Configuration settings for the database
-const dbConfig = require('./config/database.config');
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-// Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
-
-// Showing the homepage to test connection
-app.get('/', (req, res) => {
-    res.send("Welcome to Instagram! <br><br> Check /api/v1/instagram/:id to view users by ID <br> Check /api/v1/instagram/user/:name to view users by name");
-});
-
-// All the routes
-require('./app/routes/instagram.routes.js')(app);
-
+app.use('/api/v1/users/', instagram);
+app.use('/', home);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Instagram is live and listening on port ${port} :)`));
