398daf6e24b7e31725ead07157eefeba2d18ce9e
ScottRhodie <45167289+ScottRhodie@users.noreply.github.com>
2019-01-21 12:55:12 +0100

    Delete index.js

--- helpers/index.js
+++
@@ -1,7 +0,0 @@
-const findUserByName = (db, name) => db.find(currentUser => currentUser.name === name);
-const findUserById = (db, id) => db.find(currentUser => currentUser.id === parseInt(id));
-
-module.exports = {
-    findUserById,
-    findUserByName
-};
