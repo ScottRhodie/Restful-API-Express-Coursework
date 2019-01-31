f165b56816f3ccf2c491ca2fd9664713fd59a4c8
ScottRhodie <scott.rhodie@guidesmiths.com>
2019-01-31 13:35:52 +0100

    A little validation added

--- app/model.js
+++ app/model.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-    name: String,
-    photo: String,
-    likes: Number
+    name: {
+        type: String,
+        required: true,
+    },
+    photo: {
+        type: String,
+        required: true,
+    },
+    likes: {
+        type: Number,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('User', UserSchema);
