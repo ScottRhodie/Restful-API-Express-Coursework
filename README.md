a8a47541c6dbe459510c394bfcaa49477eadb472
ScottRhodie <scott.rhodie@guidesmiths.com>
2019-02-14 12:59:02 +0100

    getting set up for frontend

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Repository for uploading files that I've worked on through the course I'm doing.
 
 In previous branches there was a fully functioning program with working CRUD operations. While good initially for learning, code was all over the place and files were not doing exactly as they should be.
 
-The code is now being heavily refactored and the evolution of this app can be seen in previous commits.
+The code has now been heavily refactored and work on the front end has started.
+
+To get started with this app, please run the following script in the terminal
+
+npm run dev
--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,25 @@
-import React, { Component } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import Post from "./components/Post";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <section className="App-main">
-          <Post
-            nickname="Scott"
-            avatar="https://i.imgur.com/CjDXC37.jpg"
-            caption="Moving the community!"
-            image="https://i.imgur.com/QgvRzU3.jpg"
-          />
-          <Post
-            nickname="Blanca"
-            avatar="https://randomuser.me/api/portraits/women/46.jpg"
-            caption="Hola! Visitng Madrid!"
-            image="https://www.esmadrid.com/sites/default/files/styles/content_type_full/public/editorial/GranViaMadrid_1398771941838_1401274516.173.jpg"
-          />
-
-          {/* more posts */}
-        </section>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
