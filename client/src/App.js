163bb4a9c634a1e272ccc4d85e95897ef7eb6623
ScottRhodie <scott.rhodie@guidesmiths.com>
2019-02-14 15:55:55 +0100

    Preperation for the front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Post from "./components/Post";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <section className="App-main">
+          <Post
+            nickname="Chris the Corgi"
+            avatar="http://placecorgi.com/589/598"
+            caption="woof woof vale!"
+            image="http://placecorgi.com/1600/1600"
+          />
+          <Post
+            nickname="Dan the Dog"
+            avatar="http://placecorgi.com/178/180"
+            caption="woof woof"
+            image="http://placecorgi.com/1756/1925"
+          />
+
+          {/* more posts */}
+        </section>
       </div>
     );
   }
